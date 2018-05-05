b0b3740b716dbaee91e4a091d7ae34cfec4cb538
gp2-marr <george2.marr@live.uwe.ac.uk>
2018-05-05 13:43:53 +0100

    my php really does suck
    Signed-off-by: gp2-marr <george2.marr@live.uwe.ac.uk>

--- php/emailVerification.php
+++ php/emailVerification.php
@@ -8,9 +8,28 @@
 
 $nameErr = $emailErr;
 $name = $email = $comment;
-
+$nameRegex = '/^[a-zA-Z ]*$/';
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if(empty($_POST['name'])){
         $nameErr = 'Please input your name';
+    } else {
+        $name = test_input($_POST['name']);
+        if(!preg_match($nameRegex, $name)){
+            $nameErr = "Only letters are allowed in your name";
+        }
+    }
+
+    if(empty($_POST['email'])){
+        $emailErr = 'Email is required';
+    } else {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $emailErr = 'Invalid email format!';
+        }
+    }
+
+    if(empty($_POST['comment'])){
+        $comment = '';
+    } else {
+        $comment = test_input($_POST['comment']);
     }
 }
