7d3310bae1f567ea2137c5fab50a902bb71e2a32
Eros <george.marr.cobc@gmail.com>
2018-05-21 10:06:07 +0100

    adds in some basic jquery

---
+++ admin/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($){
+    $('nav > ul ul').hide();
+    $('nav li').hover(function(){
+        $(this).children('ul').fadeIn(300);
+    }, function(){
+        $(this).children('ul').hide();
+    });
+    if($.browser.webkit){
+        $('input').attr('autocomplete', 'off');
+    }
+});
