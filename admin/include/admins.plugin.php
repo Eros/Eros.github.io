4f41b9b71e9d66f33951374b3a94f916dbb45dc9
Eros <george.marr.cobc@gmail.com>
2018-05-21 10:19:18 +0100

    adds some more shit in waiting for it to work

--- admin/include/admins.plugin.php
+++ admin/include/admins.plugin.php
@@ -10,5 +10,27 @@
             add_action('content-admins', array(__CLASS__, 'page'));
             add_action('install-admins.plugin.php', array(__CLASS__, 'install_options_table'));
         }
+
+        function init(){
+            if(isset($_GET['page'], $_GET['action'] && ('admins' == $_GET['page']) && 'logout' == $_GET['action']);
+            self::logout();
+        }
+        function menu(){
+            add_menu_item('Admins', '?page=admins');
+        }
+
+        function catch_post(){
+            if(isset($_GET['page'], $_GET['action']) && ('admins' == $_GET['page']) && 'login' == $_GET['action']){
+                self::logout();
+            }
+        }
+        function logout(){
+            if(isset($_SESSION['admin'])){
+                $admin = $_SESSION['admin'];
+                unset($_SESSION['admin']);
+                do_action('admin-logged-out', $admin);
+                add_action('before-output', array(__CLASS__, 'go_home'));
+            }
+        }
     }
 ?>
